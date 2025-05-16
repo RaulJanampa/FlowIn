7506901d4fa60728bf2e1ab15f12face13aea33a
RaulJanampa <raul.janampa@utec.edu.pe>
2025-05-16 08:00:08 -0500

    feat: Spring Initializr

---
+++ src/test/java/com/project/FlowIn/FlowInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.FlowIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
