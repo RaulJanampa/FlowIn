d4c8359aef3fef6f196f64549639c9899fd98f5c
BecamC <alonso.bentesc@gmail.com>
2025-05-23 16:44:00 -0500

    feat: Implementación del ErroHandler en el backend. Closes #8

--- src/main/java/org/example/flowin2/web/advice/GlobalExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package org.example.flowin2.web.advice;
-
-public class GlobalExceptionHandler extends RuntimeException {
-    public GlobalExceptionHandler(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/org/example/flowin2/web/config/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package org.example.flowin2.web.config;
+
+
+import org.example.flowin2.web.config.exceptions.ResourceConflictException;
+import org.example.flowin2.web.config.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(ResourceConflictException.class)
+    public ResponseEntity<String> handleResourceConflictException(ResourceConflictException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+}
