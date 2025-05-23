3093fe200ab27a710edebb801f277af1a1fb8186
Diego Ladron de Guevara <diego.ladrondeguevar@utec.edu.pe>
2025-05-23 14:34:48 -0500

    Feat: Implemento una estructura basica para comenzar a trabajar (Feature #2

---
+++ src/test/java/org/example/flowin2/FlowIn2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.flowin2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowIn2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
