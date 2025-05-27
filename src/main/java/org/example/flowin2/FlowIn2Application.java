eb3e8edb7829496d8f350a30ee22035d2e7f7108
Diego Ladron de Guevara <diego.ladrondeguevar@utec.edu.pe>
2025-05-27 18:04:20 -0500

    Feat: se agregó una dependencia para poder usar variable de entorno y se cambio el properties

--- src/main/java/org/example/flowin2/FlowIn2Application.java
+++ src/main/java/org/example/flowin2/FlowIn2Application.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlowIn2Application {
-
     public static void main(String[] args) {
+        io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(FlowIn2Application.class, args);
     }
-
 }
